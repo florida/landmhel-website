ed04bcd2eaf285e3e3eea1e724f259be08c19c18
Florida Elago <florida.elago@gmail.com>
2013-11-06 18:18:02 -0600

    add migration

--- db/migrate/20131106053755_create_listings.rb
+++ db/migrate/20131106053755_create_listings.rb
@@ -5,13 +5,13 @@ class CreateListings < ActiveRecord::Migration
       t.string :area
       t.string :property_type
       t.string :building_type
-      t.int :no_of_bedrooms
-      t.int :no_of_bathrooms
+      t.integer :no_of_bedrooms
+      t.integer :no_of_bathrooms
       t.decimal :price
       t.boolean :featured
       t.boolean :sold
-      t.int :storeys
-      t.int :year_built
+      t.integer :storeys
+      t.integer :year_built
       t.text :description
 
       t.timestamps
