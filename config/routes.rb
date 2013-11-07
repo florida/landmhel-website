af37606c15d22b483eaf00a485ccb8f432690d9a
Florida Elago <florida.elago@gmail.com>
2013-11-07 17:57:20 -0600

    install active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Landmhel::Application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root 'home#index'
   get 'contact' => 'home#contact'
   get 'about' => 'home#about'
