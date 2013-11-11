8459887c3ae5a20481995ce4842a310f80969d78
Florida Elago <florida.elago@gmail.com>
2013-11-11 13:56:18 -0600

    add listings controller

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,19 @@
+class ListingsController < ApplicationController
+  def listings
+  end
+
+  def listings_details
+  end
+
+  def sales
+  end
+
+  def sales_details
+  end
+
+  private 
+
+  def set_listing
+
+  end
+end
