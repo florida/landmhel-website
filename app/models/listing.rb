e7c8871857d7285272c0a5566fd8479bccc58729
Florida Elago <florida.elago@gmail.com>
2013-11-11 13:54:40 -0600

    add to_s

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,8 @@
 class Listing < ActiveRecord::Base
+  has_many :images, foreign_key: :listing_id, dependent: :destroy
+  accepts_nested_attributes_for :images
+
+  def to_s 
+    return address;
+  end
 end
