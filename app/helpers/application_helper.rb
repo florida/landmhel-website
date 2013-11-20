b4877597362863068515736b23bdbeea88016171
Florida Elago <florida.elago@gmail.com>
2013-11-20 16:00:51 -0600

    add label and value helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def label_tag(name, additional_tags = "")
+    "<span class='listing-label #{additional_tags}'>#{name}: </span>".html_safe
+  end
+
+  def value_tag(name, additional_tags = "")
+    "<span class='listing-value #{additional_tags}'>#{name}</span>".html_safe
+  end
 end
