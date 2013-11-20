d36617c7adc9c759229e74ae2a526c5b029c575a
Florida Elago <florida.elago@gmail.com>
2013-11-20 15:59:32 -0600

    load featured listings and add more actions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,15 @@
 class HomeController < ApplicationController
   def index
+    @featured_listings = Listing.all
   end
 
   def contact
   end
 
   def about
+  end
+
+  def our_team
   end
 
   def sample
