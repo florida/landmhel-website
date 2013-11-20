c01003d7f185c9f132fe9a5c874d9ee2220f2d98
Florida Elago <florida.elago@gmail.com>
2013-11-20 16:00:32 -0600

    load listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,11 +1,14 @@
 class ListingsController < ApplicationController
   def listings
+    @listings = Listing.active
   end
 
   def listings_details
+    @listing = Listing.find(params[:id])
   end
 
   def sales
+    @sales = Listing.sold
   end
 
   def sales_details
