8840acf1032d7aee9a274b068dbe24d85dc926c4
Florida Elago <florida.elago@gmail.com>
2013-11-20 16:02:51 -0600

    remove return

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,21 @@
 class Listing < ActiveRecord::Base
   has_many :images, foreign_key: :listing_id, dependent: :destroy
+  belongs_to :agent
+
   accepts_nested_attributes_for :images
 
+  scope :sold, -> { where(sold: true) }
+  scope :featured, -> { where(featured: true) }
+  scope :active, -> { where(sold: false) }
+
+  geocoded_by :full_address
+  after_validation :geocode, :if => :address_changed?
+
   def to_s 
-    return address;
+    address
+  end
+
+  def full_address
+    "#{address}, #{city}, #{province}"
   end
 end
