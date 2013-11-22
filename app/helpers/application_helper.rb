f27019f34a4dc668d90c3c6f96b81192d3db37e0
Florida Elago <florida.elago@gmail.com>
2013-11-22 16:04:50 -0600

    add active classes if links are active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def value_tag(name, additional_tags = "")
     "<span class='listing-value #{additional_tags}'>#{name}</span>".html_safe
   end
+
+  def is_active?(link)
+    return "active" if params[:action] == link
+  end
 end
