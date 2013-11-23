a42f2eb7e6865d9a109626b22ebb5b6dca6afdcb
Florida Elago <florida.elago@gmail.com>
2013-11-23 15:13:40 -0600

    remove scale

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -12,6 +12,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+
+  process :quality => 100
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -28,19 +30,19 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :large do
-    process :scale => [250, 50]
-  end
+  # version : do
+  #   process :scale => [250, 50]
+  # end
 
-  version :medium do
-    process :scale => [250, 50]
-  end
+  # version :medium do
+  #   process :scale => [250, 50]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
