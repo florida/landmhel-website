51a31fff90ad8ccce0ac8696b65464210ab662d3
Florida Elago <florida.elago@gmail.com>
2013-11-24 00:15:35 -0600

    remove quality

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -12,8 +12,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-
-  process :quality => 100
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
