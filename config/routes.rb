7cca5a3b19b81c998e57ce3892d1c6ab14419a75
Florida Elago <florida.elago@gmail.com>
2013-11-27 01:06:12 -0600

    post to contact

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Landmhel::Application.routes.draw do
   get 'services' => 'home#services'
   get 'our_team' => 'home#our_team'
   get 'sample' => 'home#sample'
+  post 'contact' => 'home#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
