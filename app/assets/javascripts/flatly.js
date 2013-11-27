a8e67d1df558bf44c427e3a58542dcc3bda3797e
Florida Elago <florida.elago@gmail.com>
2013-11-26 23:32:13 -0600

    add agent id

--- app/assets/javascripts/flatly.js
+++ app/assets/javascripts/flatly.js
@@ -10,6 +10,10 @@ jQuery(document).ready(function ($) {
   ['property_type', 'area', 'style', 'province', 'agent', 'price', 'bedrooms', 'bathrooms', 'min_bedrooms', 'min_bathrooms'].forEach(function(select){
     $('#' + select).prop('selectedIndex', -1);
   });
+
+  $('.contact-agent').click(function() {
+    $("#agent_id").val($(this).attr('data-agent-id'));
+  });
   
 });
 
