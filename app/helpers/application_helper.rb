7f389fd47d43adc7159a8f83670ee86a729f206d
Florida Elago <florida.elago@gmail.com>
2013-12-04 22:41:35 -0600

    add helpers for description and keywords

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,16 @@ module ApplicationHelper
   def title(page_title)
      content_for :title, page_title
   end
+
+  def description(page_description)
+    content_for :description, truncate(page_description,255)
+  end
+
+  def keywords(page_keywords)
+    content_for :keywords, truncate(page_keywords,255)
+  end
+
+  def site_image(image)
+    content_for :site_image, image
+  end
 end
