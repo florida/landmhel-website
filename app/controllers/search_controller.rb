5de55816ae7387b96b452344c6b4805ff365805b
Florida Elago <florida.elago@gmail.com>
2013-12-05 16:54:31 -0600

    remove debugger

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,9 @@
-require 'debugger'
 class SearchController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:query]
   def query
     query = params[:query]
     
     @listings = Listing.where("address like ? or description like ?", "%#{query}%", "%#{query}%")
-    debugger
     if !@listings.blank? 
       @title = "Results for #{query}"
     else 
