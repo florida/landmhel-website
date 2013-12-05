6f1761ee85017421216893b555efff724354b17a
Florida Elago <florida.elago@gmail.com>
2013-12-05 16:45:39 -0600

    fix search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,16 @@
+require 'debugger'
 class SearchController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:query]
   def query
-      query = params[:query]
-      
-      @listings = Listing.where("address like ? or description like ?", "%#{query}%", "%#{query}%")
-
-      if @listings.blank? 
-        @title = "Results for #{query}"
-      else 
-        @title = 'No Results Found' 
-        @listings = Listing.all
-      end
+    query = params[:query]
+    
+    @listings = Listing.where("address like ? or description like ?", "%#{query}%", "%#{query}%")
+    debugger
+    if !@listings.blank? 
+      @title = "Results for #{query}"
+    else 
+      @title = 'No Results Found' 
+      @listings = Listing.all
     end
   end
-
 end
