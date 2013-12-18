59ca6acfe3917ce3a7f81a725860407c02e57883
Florida Elago <florida.elago@gmail.com>
2013-12-17 18:35:48 -0600

    add settings logic

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+class Settings < Settingslogic
+  source "config/settings.yml"
+  namespace Rails.env
+end
+
 module Landmhel
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
-    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],
+    :aws_access_key_id      => Settings.aws_access_key_id,
+    :aws_secret_access_key  => Settings.aws_secret_access_key,
   }
 
-  config.fog_directory = ENV['AWS_BUCKET_NAME']
+  config.fog_directory = Settings.aws_bucket_name
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} 
 
   if Rails.env.test? || Rails.env.cucumber?
