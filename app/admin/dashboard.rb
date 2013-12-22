eceb6ed999bf21c42e93a88d68316b95782a8f3e
Florida Elago <florida.elago@gmail.com>
2013-12-22 15:35:49 -0600

    update dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,28 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Recent Inquiries" do   
+          table_for Inquiry.recent(10).map do 
+            column :name do |name|
+              link_to(name.name, [:admin, name])
+            end
+            column :comment
+          end
+        end
+      end
+
+      column do
+        panel "Recent Listings" do
+          table_for Listing.recent(5).map do 
+            column :address do |listing|
+              link_to(listing.address, [:admin, listing])
+            end
+            column :agent
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
