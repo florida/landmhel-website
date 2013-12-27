73f46f3fd568d423c43c51875a4f315daca40083
Florida Elago <florida.elago@gmail.com>
2013-12-27 02:01:08 -0600

    add agent factory

--- test/factories/agent.rb
+++ test/factories/agent.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
   factory :agent do
-        
+    sequence(:first_name) { |n| "Agent#{n}" }
+    sequence(:last_name) { |n| "Last#{n}" }
+    sequence(:phone, (1000..9999).cycle) { |n| "204555#{n}".to_i }
+    sequence(:email) { |n| "agent#{n}@example.com" }
+    description "Lorem ipsum dolor sit amet, eos saperet legendos necessitatibus ad, in cum vidisse ornatus repudiandae. Ei sit paulo quaerendum, ex illud populo possit eos. Elitr altera malorum sit ea, vel nostro delenit cu. Erant minimum volutpat et mea. Eu agam veniam utamur quo, an vel habeo maluisset democritum, an eos mandamus theophrastus. Laudem dicunt no cum, at quidam ancillae eum."
+    sequence(:facebook_url) { |n| "http://www.facebook.com/page#{n}" }
+    sequence(:twitter_url) { |n| "http://www.twitter.com/user#{n}" }
+    sequence(:linkedin_url) { |n| "http://www.linkedin.com/user#{n}" }
+    sequence(:website_url) { |n| "http://www.website#{n}.com" }
   end
 end
