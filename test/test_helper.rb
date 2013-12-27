143143765a0d5db71222c1843f6f375eb69f2ff1
Florida Elago <florida.elago@gmail.com>
2013-12-27 00:43:28 -0600

    require minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,8 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
-class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
-end
+require 'minitest/rails'
+require 'minitest/pride'
+require 'minitest/spec'
+require 'mocha/setup'
+require 'minitest/autorun'
