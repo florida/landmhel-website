f35a8917a2f35fbf49986d4c1fd131d1b1c6bfc5
Florida Elago <florida.elago@gmail.com>
2013-12-27 00:42:40 -0600

    add listing factory

---
+++ test/factories/listing.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :listing do
+    sequence(:address, (201..3755).cycle) {|n| "#{n} Portage Ave."}
+    sequence(:area, ListingData::AREAS.cycle) {|n| n}
+    sequence(:property_type, ListingData::PROPERTY_TYPES.cycle) {|n| n}
+    sequence(:style, ListingData::STYLES.cycle) {|n| n}
+    city "Winnipeg"
+    province "MB"
+    bedrooms { rand(1..10) }
+    bathrooms { rand(1..10) }
+    price { rand(100000..500000) }
+    sequence(:featured, [true, false].cycle) {|n| n}
+    sequence(:sold, [true, false].cycle) {|n| n}
+    year_built { rand(1800, 2013) }
+  end
+end
