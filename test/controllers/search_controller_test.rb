e9a4c647bd8a469ca6a7ed3ab8aa545c21d9a66b
Florida Elago <florida.elago@gmail.com>
2013-12-28 21:05:09 -0600

    change to spec

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -1,14 +1,8 @@
 require "test_helper"
 
-class SearchControllerTest < ActionController::TestCase
-  test "should get query" do
+describe SearchController do
+  it "should return success on query" do
     get :query
     assert_response :success
   end
-
-  test "should get filter" do
-    get :filter
-    assert_response :success
-  end
-
 end
