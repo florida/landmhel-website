5ce4f01f19f8cf9a7b202d8825975c555f1c0500
Florida Elago <florida.elago@gmail.com>
2013-12-28 20:43:05 -0600

    add tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,19 +1,38 @@
 require "test_helper"
 
-class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
+describe HomeController do
+  [:index, :contact, :services, :about, :our_team, :sitemap].each do |page|
+    it "should return success on #{page}" do
+      get page
+      assert_response :success
+    end
+  end
+
+  [:services, :about, :contact, :our_team].each do |page|
+    it "should invoke #initialize_inquiry on #{page}" do
+      get page
+      @controller.instance_variable_get("@inquiry").must_be_instance_of(Inquiry)
+    end
+  end
+
+  it "should get featured listings on index" do
+    Listing.expects(:featured)
     get :index
-    assert_response :success
   end
 
-  test "should get contact" do
+  it "should get all listings on contact" do
+    Listing.expects(:all)
     get :contact
-    assert_response :success
   end
 
-  test "should get about" do
-    get :about
-    assert_response :success
+  it "should get all agent on our_team" do
+    Agent.expects(:all)
+    get :our_team
+  end
+
+  it "should get all listings on sitemap" do
+    Listing.expects(:all)
+    get :sitemap
   end
 
 end
