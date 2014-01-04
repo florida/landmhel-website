ce0c3c8eb704a72193b4d3951b5ac4f949f289a2
Florida Elago <florida.elago@gmail.com>
2014-01-04 01:05:39 -0600

    remove code from action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,6 @@
 class SearchController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:query]
   def query
-    query = params[:query]
-    
-    @listings = Listing.where("address like ? or description like ?", "%#{query}%", "%#{query}%")
-    if !@listings.blank? 
-      @title = "Results for #{query}"
-    else 
-      @title = 'No Results Found' 
-      @listings = Listing.all
-    end
+
   end
 end
