37531c685214f7ff000e057463b9eea0ac6afa33
Florida Elago <florida.elago@gmail.com>
2014-01-05 01:32:10 -0600

    validate presence of name

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,5 +1,7 @@
 class Inquiry < ActiveRecord::Base
   belongs_to :agent
   belongs_to :listing
+  validates :name, presence: true
+  
   scope :recent, ->(limit = 10) { order("created_at desc").limit(limit) }
 end
