3ab7075c358f2ec620e9d355fc551da6da367e3c
Florida Elago <florida.elago@gmail.com>
2014-01-19 15:29:01 -0600

    remove surrounding document.ready

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,14 +4,10 @@
 //= require flatly/loader
 //= require flatly/bootswatch 
 
+['property_type', 'area', 'style', 'province', 'agent', 'price', 'bedrooms', 'bathrooms', 'min_bedrooms', 'min_bathrooms'].forEach(function(select){
+  $('#' + select).prop('selectedIndex', -1);
+});
 
-jQuery(document).ready(function ($) {
-  ['property_type', 'area', 'style', 'province', 'agent', 'price', 'bedrooms', 'bathrooms', 'min_bedrooms', 'min_bathrooms'].forEach(function(select){
-    $('#' + select).prop('selectedIndex', -1);
-  });
-
-  $('.contact-agent').click(function() {
-    $("#inquiry_agent_id").val($(this).attr('data-agent-id'));
-  });
-  
+$('.contact-agent').click(function() {
+  $("#inquiry_agent_id").val($(this).attr('data-agent-id'));
 });
