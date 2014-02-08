35eae4ebd98138a07437ba42af282d4987e6d8e6
Florida Elago <florida.elago@gmail.com>
2014-02-08 13:35:28 -0600

    fix config deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :deploy_to, '/home/deploy/landmhel'
 # set :pty, true
 
 set :linked_files, %w{config/database.yml config/settings.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
