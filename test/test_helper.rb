c08f14a8d73244cee01cf4d0de6a7e90a8817eab
Florida Elago <florida.elago@gmail.com>
2014-02-07 19:47:23 -0600

    disable resque in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
+Resque.inline = ENV['RAILS_ENV'] == "test"
 require 'rails/test_help'
 require 'minitest/rails'
 require 'minitest/pride'
