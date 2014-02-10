2d08260c5e92fefedd7add04b19250c1e40b5e78
Florida Elago <florida.elago@gmail.com>
2014-02-09 18:54:52 -0600

    process in background

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -2,7 +2,7 @@ class Agent < ActiveRecord::Base
   has_many :listings, dependent: :destroy
   has_many :inquiries, dependent: :destroy
   mount_uploader :avatar, AvatarUploader
-
+  process_in_background :avatar
   def to_s 
     return "#{first_name} #{last_name}"
   end
